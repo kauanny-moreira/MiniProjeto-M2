677ff749f7f692fb6cf6adc6f9dd0111cc8b1be3
kauanny-moreira <kauannymoreira82@gmail.com>
2025-04-01 20:58:50 -0300

    Criação da primeira pag

---
+++ script.js
@@ -0,0 +1,11 @@
+// Cadastro de jogadores de futebol numa liga de futebol
+
+// const paragrafo = document.createElement("p");
+// // adicionando ao body
+// document.body.appendChild(paragrafo);
+// paragrafo.innerText = "Texto alterado pelo JavaScript!"
+
+// const titulo = document.createElement("h1");
+// document.body.appendChild(titulo);
+// titulo.innerText = "Liga de Futsal 'One-One'";
+
